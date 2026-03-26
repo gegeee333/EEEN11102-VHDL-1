64f579a5b4c0dc184b5952c9db33cb8dd9c92de2
gegeee333 <peter.heywood-3@student.manchester.ac.uk>
2026-03-26 15:20:12 +0000

    d

--- VHDL_Labs/Tutorial/circuit_tb.vhd
+++ VHDL_Labs/Tutorial/circuit_tb.vhd
@@ -33,3 +33,31 @@ begin
     wait for 100 ns;
   end process;
 end;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity circuit_tb is
+end;
+
+architecture bench of circuit_tb is
+  -- Clock period
+  constant clk_period : time := 5 ns;
+  -- Generics
+  -- Ports
+  signal a : std_logic;
+  signal b : std_logic;
+  signal enable : std_logic;
+  signal y : std_logic;
+begin
+
+  circuit_inst : entity work.circuit
+  port map (
+    a => a,
+    b => b,
+    enable => enable,
+    y => y
+  );
+-- clk <= not clk after clk_period/2;
+
+end;
