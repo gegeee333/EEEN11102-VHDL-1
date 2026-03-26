4fd7b36a6ea2e8119ed85bf662265e45c74f62cf
gegeee333 <peter.heywood-3@student.manchester.ac.uk>
2026-03-26 15:08:26 +0000

    chang

--- VHDL_Labs/Tutorial/circuit_tb.vhd
+++ VHDL_Labs/Tutorial/circuit_tb.vhd
@@ -10,25 +10,26 @@ architecture bench of circuit_tb is
   -- Clock period
   -- Generics
   -- Ports
-  signal a : std_logic;
-  signal b : std_logic;
+  signal a      : std_logic;
+  signal b      : std_logic;
   signal enable : std_logic;
-  signal y : std_logic;
+  signal y      : std_logic;
 begin
 
   circuit_inst : entity work.circuit
-  port map (
-    a => a,
-    b => b,
-    enable => enable,
-    y => y
-  );
--- clk <= not clk after clk_period/2;
-process 
-begin 
-a   <= '0', '1' after 20 ns;
-b   <= '0', '1' after 40 ns; 
-enable <= '0', '1' after 60 ns, '0' after 80 ns; 
-wait for 100 ns; 
-end process;
+    port map
+    (
+      a      => a,
+      b      => b,
+      enable => enable,
+      y      => y
+    );
+  -- clk <= not clk after clk_period/2;
+  process
+  begin
+    a      <= '0', '1' after 20 ns;
+    b      <= '0', '1' after 40 ns;
+    enable <= '0', '1' after 60 ns, '0' after 80 ns;
+    wait for 100 ns;
+  end process;
 end;
